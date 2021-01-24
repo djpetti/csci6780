0041244a6b1bc60d678e3b70d4566e71328705a1
Daniel Petti <djpetti@gmail.com>
2021-01-24 16:36:44 -0500

    Fix minor issues in file_handler.h.

--- project1/server/file_handler/file_handler.h
+++ project1/server/file_handler/file_handler.h
@@ -9,21 +9,20 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-namespace server {
-namespace file_handler {
+namespace server::file_handler {
 
 /**
  * @brief Common interface for filesystem operations.
  */
 class IFileHandler {
+ public:
   /**
    * @brief Gets the contents of a file in the current remote directory on the
    *    server.
    * @param filename The name of the file to get.
    * @return The binary contents of the file.
    */
-  virtual vector<uint8_t> Get(const string& filename) const = 0;
+  virtual std::vector<uint8_t> Get(const std::string& filename) const = 0;
 
   /**
    * @brief Creates a new file in the current remote directory on the server.
@@ -31,28 +30,28 @@ class IFileHandler {
    * @param contents The binary contents of the new file.
    * @return True on success, false on failure.
    */
-  virtual bool Put(const string& filename,
-                   const vector<uint8_t>& contents) = 0;
+  virtual bool Put(const std::string& filename,
+                   const std::vector<uint8_t>& contents) = 0;
 
   /**
    * @brief Deletes a file in the current remote directory on the server.
    * @param filename The name of the file to delete.
    * @return True on success, false on failure.
    */
-  virtual bool Delete(const string& filename) = 0;
+  virtual bool Delete(const std::string& filename) = 0;
 
   /**
    * @brief Lists the files in the current remote directory.
    * @return The list of files on the remote.
    */
-  virtual std::vector<string> List() const = 0;
+  virtual std::vector<std::string> List() const = 0;
 
   /**
    * @brief Changes to a new directory that is a sub-folder of the current one.
    * @param sub_folder The sub-folder to enter.
    * @return True on success, false on failure.
    */
-  virtual bool ChangeDir(const string& sub_folder) = 0;
+  virtual bool ChangeDir(const std::string& sub_folder) = 0;
 
   /**
    * @brief Moves to the parent of the current directory.
@@ -65,7 +64,7 @@ class IFileHandler {
    * @param name The name of the sub-directory.
    * @return True on success, false on failure.
    */
-  virtual bool MakeDir(const string& name) = 0;
+  virtual bool MakeDir(const std::string& name) = 0;
 
   /**
    * @return The full path to the current remote directory.
@@ -73,7 +72,6 @@ class IFileHandler {
   virtual std::string GetCurrentDir() const = 0;
 };
 
-}  // namespace file_handler
 }  // namespace server
 
 #endif  // PROJECT1_FILE_HANDLER_H
