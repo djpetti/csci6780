973a96e38ec13b1822e7348ca0136f1ae25860a0
Daniel Petti <djpetti@gmail.com>
2021-02-27 12:53:25 -0500

    Add logging to thread pool.

---
+++ project1/loguru.cmake
@@ -0,0 +1,25 @@
+# Download and unpack loguru at configure time
+configure_file(loguru-download.cmake.in loguru-download/CMakeLists.txt)
+execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+        RESULT_VARIABLE result
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/loguru-download )
+if(result)
+    message(FATAL_ERROR "CMake step for loguru failed: ${result}")
+endif()
+execute_process(COMMAND ${CMAKE_COMMAND} --build .
+        RESULT_VARIABLE result
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/loguru-download )
+if(result)
+    message(FATAL_ERROR "Build step for loguru failed: ${result}")
+endif()
+
+# Prevent overriding the parent project's compiler/linker
+# settings on Windows
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+# Add loguru directly to our build.
+add_library(loguru ${CMAKE_CURRENT_BINARY_DIR}/loguru-src/loguru.cpp)
+# Make sure all dependencies can find the header.
+target_include_directories(loguru INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/loguru-src)
+# Enable logging with streams by default.
+target_compile_definitions(loguru PUBLIC LOGURU_WITH_STREAMS=1)
--- project1/thread_pool/thread_pool.cpp
+++ project1/thread_pool/thread_pool.cpp
@@ -1,6 +1,6 @@
 #include "thread_pool.h"
 
-#include <cassert>
+#include <loguru.hpp>
 
 namespace thread_pool {
 
@@ -10,6 +10,8 @@ ThreadPool::ThreadPool(uint32_t num_threads)
       max_pool_size_(num_threads) {}
 
 ThreadPool::~ThreadPool() {
+  LOG_S(INFO) << "Closing thread pool.";
+
   {
     std::lock_guard<std::mutex> lock(mutex_);
 
@@ -21,6 +23,7 @@ ThreadPool::~ThreadPool() {
     // Indicate that we should stop the pool threads.
     should_close_ = true;
   }
+  LOG_S(1) << "Joining internal threads...";
   // Wake up the internal threads and force them to check should_close_.
   task_pending_.notify_one();
   task_done_.notify_one();
@@ -29,6 +32,7 @@ ThreadPool::~ThreadPool() {
   dispatcher_thread_.join();
   joiner_thread_.join();
 
+  LOG_S(1) << "Joining task threads...";
   // No need to hold the mutex for this since no other threads are running at
   // this point.
   for (auto& handle_and_thread : handle_to_thread_) {
@@ -38,6 +42,7 @@ ThreadPool::~ThreadPool() {
 
 Task::Handle ThreadPool::AddTask(std::unique_ptr<Task>* task) {
   const auto kTaskHandle = (*task)->GetHandle();
+  LOG_S(INFO) << "Adding a new task with handle " << kTaskHandle << ".";
 
   {
     std::lock_guard<std::mutex> lock(mutex_);
@@ -65,25 +70,30 @@ void ThreadPool::DispatcherThread() {
       // Check that we have a free thread.
       if (max_pool_size_ != 0 && pool_size_ >= max_pool_size_) {
         // Wait for a thread to become available.
+        LOG_S(1) << "Waiting for a free thread...";
         thread_available_.wait(lock,
                                [this] { return pool_size_ < max_pool_size_; });
       }
 
       // Wait for a new pending task.
       if (dispatch_queue_.empty() && !should_close_) {
+        LOG_S(1) << "Waiting for a new task...";
         task_pending_.wait(
             lock, [this] { return !dispatch_queue_.empty() || should_close_; });
       }
       if (should_close_) {
         // We should exit now and avoid submitting new tasks.
+        LOG_S(INFO) << "Exiting dispatcher thread.";
         break;
       }
 
       ++pool_size_;
+      LOG_S(2) << "Pool size is now " << pool_size_ << ".";
 
       // Create a thread for the task.
       const auto kTaskHandle = dispatch_queue_.back();
       dispatch_queue_.pop();
+      LOG_S(1) << "Got a new task: " << kTaskHandle << ".";
 
       const auto& task = handle_to_task_[kTaskHandle];
       std::thread task_thread(&ThreadPool::RunTask, this, task.get());
@@ -98,17 +108,20 @@ void ThreadPool::JoinerThread() {
 
       // Wait for a new done task.
       if (joinable_queue_.empty() && !should_close_) {
+        LOG_S(1) << "Waiting for a task to finish...";
         task_done_.wait(
             lock, [this] { return !joinable_queue_.empty() || should_close_; });
       }
       if (should_close_) {
         // We should exit now and avoid finalizing new tasks.
+        LOG_S(INFO) << "Exiting joiner thread.";
         break;
       }
 
       // Finalize the task.
       const auto kTaskHandle = joinable_queue_.back();
       joinable_queue_.pop();
+      LOG_S(1) << "Got a finished task: " << kTaskHandle << ".";
 
       auto& thread = handle_to_thread_[kTaskHandle];
       thread.join();
@@ -122,6 +135,7 @@ void ThreadPool::JoinerThread() {
 
       // Indicate that we have a free thread.
       --pool_size_;
+      LOG_S(2) << "Pool size is now " << pool_size_ << ".";
     }
     thread_available_.notify_one();
   }
@@ -133,6 +147,7 @@ bool ThreadPool::UpdateTaskStatus(Task::Handle handle, Task::Status status) {
 
     // Check if the task should be cancelled.
     if (cancelled_tasks_.find(handle) != cancelled_tasks_.end()) {
+      LOG_S(INFO) << "Task " << handle << " has been cancelled.";
       status = Task::Status::CANCELLED;
     }
 
@@ -140,20 +155,26 @@ bool ThreadPool::UpdateTaskStatus(Task::Handle handle, Task::Status status) {
     handle_to_status_[handle] = status;
   }
 
+  LOG_IF_S(ERROR, status == Task::Status::FAILED)
+      << "Task " << handle << " failed!";
+
   // Indicate whether we should exit or not.
   return status == Task::Status::RUNNING;
 }
 
 void ThreadPool::RunTask(Task* task) {
   // Perform one-time setup.
+  LOG_S(1) << "Performing setup for task " << task->GetHandle() << ".";
   Task::Status status = task->SetUp();
 
   // Run the task.
   while (UpdateTaskStatus(task->GetHandle(), status)) {
+    LOG_S(1) << "Running one iteration of task " << task->GetHandle() << ".";
     status = task->RunAtomic();
   }
 
   // The task is finished. Perform cleanup.
+  LOG_S(1) << "Cleaning up task " << task->GetHandle() << ".";
   task->Cleanup();
 
   // Notify that the task is complete.
@@ -169,13 +190,15 @@ Task::Status ThreadPool::GetTaskStatus(const Task::Handle& task_handle) {
   std::lock_guard<std::mutex> lock(mutex_);
 
   const auto handle_and_status = handle_to_status_.find(task_handle);
-  assert(handle_and_status != handle_to_status_.end() &&
-         "Attempt to get status of nonexistent thread.");
+  CHECK_S(handle_and_status != handle_to_status_.end())
+      << "Attempt to get status of nonexistent thread.";
   return handle_to_status_[task_handle];
 }
 
 void ThreadPool::CancelTask(const Task::Handle& task_handle) {
   std::lock_guard<std::mutex> lock(mutex_);
+
+  LOG_S(INFO) << "Cancelling task " << task_handle << ".";
   cancelled_tasks_.insert(task_handle);
 }
 
