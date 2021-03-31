8e2652e50e4d6a959df8bf665520486f5f1b2859
Nicholas Stonecipher <nickster258@users.noreply.github.com>
2021-03-30 21:26:10 -0400

    Resolve memory leak issue (x2)

--- project3/participant/input_parser/input_parser.cpp
+++ project3/participant/input_parser/input_parser.cpp
@@ -3,7 +3,15 @@
 namespace participant::input_parser {
 
 InputParser::InputParser()
-    : multi_id_{0}, is_valid_{true}, req_{REG}, message_{} {}
+    : reg_msg_{pub_sub_messages::Register().New()},
+      dereg_msg_{pub_sub_messages::Deregister().New()},
+      discon_msg_{pub_sub_messages::Disconnect().New()},
+      recon_msg_{pub_sub_messages::Reconnect().New()},
+      msend_msg_{pub_sub_messages::SendMulticast().New()},
+      multi_id_{0},
+      is_valid_{true},
+      req_{REG},
+      message_{} {}
 
 void InputParser::Parse(std::string& cmd) {
   std::istringstream iss(cmd);
@@ -45,23 +53,18 @@ google::protobuf::Message* InputParser::CreateReq() {
   return nullptr;
 }
 pub_sub_messages::Register* InputParser::CreateRegMsg() {
-  reg_msg_ = pub_sub_messages::Register().New();
   return reg_msg_;
 }
 pub_sub_messages::Deregister* InputParser::CreateDeregMsg() {
-  dereg_msg_ = pub_sub_messages::Deregister().New();
   return dereg_msg_;
 }
 pub_sub_messages::Disconnect* InputParser::CreateDisconMsg() {
-  discon_msg_ = pub_sub_messages::Disconnect().New();
   return discon_msg_;
 }
 pub_sub_messages::Reconnect* InputParser::CreateReconMsg() {
-  recon_msg_ = pub_sub_messages::Reconnect().New();
   return recon_msg_;
 }
 pub_sub_messages::SendMulticast* InputParser::CreateMsendMsg() {
-  msend_msg_ = pub_sub_messages::SendMulticast().New();
   msend_msg_->set_message(message_);
   return msend_msg_;
 }
