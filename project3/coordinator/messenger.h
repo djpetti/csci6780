0ba15c7dcad114dcb10b86244e8f5f8b9198934f
Jake Chandler <jake.chandler@uga.edu>
2021-04-01 19:52:44 -0400

    Add registrar and messenger files.

---
+++ project3/coordinator/messenger.h
@@ -0,0 +1,16 @@
+/**
+ * @File Messenger to be utilized by the coordinator.
+ */
+
+#ifndef PROJECT3_MESSENGER_H
+#define PROJECT3_MESSENGER_H
+
+namespace coordinator::messenger {
+/**
+ * @class Handles sending broadcasting to participants.
+ */
+    class Messenger {
+
+    }; // Class
+}// namespace
+#endif //PROJECT3_MESSENGER_H
---
+++ project3/coordinator/registrar.h
@@ -0,0 +1,17 @@
+/**
+ * @file Registrar to be utilized by the coordinator.
+ */
+
+#ifndef PROJECT3_REGISTRAR_H
+#define PROJECT3_REGISTRAR_H
+
+namespace coordinator::registrar {
+
+    /**
+    * @class Handles registration/de-registration of participants.
+    */
+    class Registrar {
+
+    };// Class
+}// namespace
+#endif //PROJECT3_REGISTRAR_H
