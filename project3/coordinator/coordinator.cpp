dcc4f4a6a5be577c365e9fffe3aab27a01e65998
Jake Chandler <jake.chandler@uga.edu>
2021-04-05 17:21:41 -0400

    fix parameter types for SendMessage and SerializeMessage.

--- project3/coordinator/coordinator.cpp
+++ project3/coordinator/coordinator.cpp
@@ -41,22 +41,27 @@ bool Coordinator::Coordinate() {
   return client_state != ClientState::ERROR;
 }
 
-void Coordinator::DetermineParticipant() {
+ConnectedParticipants::Participant Coordinator::DetermineParticipant(
+    uint32_t id) {
   // determine the participant.
   for (auto participant :
-      registrar_->GetConnectedParticipants()->GetParticipants()) {
-    if (participant.hostname == hostname_) {
-      participant_ = participant;
+       registrar_->GetConnectedParticipants()->GetParticipants()) {
+    if (participant.id == id) {
+      return participant;
     }
   }
+  LOG_F(ERROR, "Error determining participant for client.");
+  return ConnectedParticipants::Participant();
 }
-void Coordinator::DetermineMessenger() {
+std::shared_ptr<Messenger> Coordinator::DetermineMessenger(uint32_t id) {
   // determine this participant's messenger.
   for (auto messenger : messenger_mgr_->GetMessengers()) {
-    if (messenger->GetParticipant().hostname == hostname_) {
-      messenger_ = messenger;
+    if (messenger->GetParticipant().id == id) {
+      return messenger;
     }
   }
+  LOG_F(ERROR, "Error determining messenger for client.");
+  return std::shared_ptr<Messenger>();
 }
 
 Coordinator::ClientState Coordinator::ReadNextMessage(
@@ -99,30 +104,14 @@ Coordinator::ClientState Coordinator::ReadNextMessage(
 Coordinator::ClientState Coordinator::DispatchMessage(
     const pub_sub_messages::CoordinatorMessage &message) {
   if (message.has_register_()) {
-    LOG_F(INFO, "Handling a register command from Participant %i.",
-          participant_.id);
     return HandleRequest(message.register_());
   } else if (message.has_deregister()) {
-    DetermineParticipant();
-    DetermineMessenger();
-    LOG_F(INFO, "Handling a deregister command from Participant %i.",
-          participant_.id);
     return HandleRequest(message.deregister());
   } else if (message.has_disconnect()) {
-    DetermineParticipant();
-    LOG_F(INFO, "Handling a disconnect command from Participant %i.",
-          participant_.id);
     return HandleRequest(message.disconnect());
   } else if (message.has_reconnect()) {
-    DetermineParticipant();
-    DetermineMessenger();
-    LOG_F(INFO, "Handling a reconnect command from Participant %i.",
-          participant_.id);
     return HandleRequest(message.reconnect());
   } else if (message.has_send_multicast()) {
-    DetermineParticipant();
-    LOG_F(INFO, "Handling a msend command from Participant %i.",
-          participant_.id);
     return HandleRequest(message.send_multicast());
   }
   LOG_F(ERROR, "No valid message from client (%i) was recieved.", client_fd_);
@@ -132,56 +121,80 @@ Coordinator::ClientState Coordinator::DispatchMessage(
 Coordinator::ClientState Coordinator::HandleRequest(
     const pub_sub_messages::Register &request) {
   // create this participant and register with the registrar.
-  participant_.id = GenerateID();
-  participant_.hostname = hostname_;
-  participant_.port = request.port_number();
+  ConnectedParticipants::Participant participant;
+  participant.id = GenerateID();
+  participant.hostname = hostname_;
+  participant.port = request.port_number();
+  LOG_F(INFO, "Handling a register command from Participant %i.",
+        participant.id);
 
-  registrar_->RegisterParticipant(participant_);
+  registrar_->RegisterParticipant(participant);
   // initialize this participant's Messenger and register it with the messenger
   // manager.
-  messenger_ = std::make_shared<Messenger>(msg_log_, participant_);
-  messenger_mgr_->AddMessenger(messenger_);
+  auto messenger = std::make_shared<Messenger>(msg_log_, participant);
+  messenger_mgr_->AddMessenger(messenger);
 
   pub_sub_messages::RegistrationResponse response;
-  SendRegistrationResponse(response);
+  SendRegistrationResponse(response, participant);
   return ClientState::ACTIVE;
 }
 Coordinator::ClientState Coordinator::HandleRequest(
     const pub_sub_messages::Deregister &request) {
-  registrar_->DeregisterParticipant(participant_);
-  messenger_mgr_->DeleteMessenger(messenger_);
+  DetermineParticipant(request.participant_id());
+  DetermineMessenger(request.participant_id());
+  LOG_F(INFO, "Handling a deregister command from Participant %i.",
+        request.participant_id());
+  auto participant = DetermineParticipant(request.participant_id());
+  auto messenger = DetermineMessenger(request.participant_id());
+  registrar_->DeregisterParticipant(participant);
+  messenger_mgr_->DeleteMessenger(messenger);
   return ClientState::ACTIVE;
 }
 
 Coordinator::ClientState Coordinator::HandleRequest(
     const pub_sub_messages::Disconnect &request) {
-  registrar_->DisconnectParticipant(participant_);
+  DetermineParticipant(request.participant_id());
+  LOG_F(INFO, "Handling a disconnect command from Participant %i.",
+        request.participant_id());
+  auto participant = DetermineParticipant(request.participant_id());
+  registrar_->DisconnectParticipant(participant);
   return ClientState::ACTIVE;
 }
 
 Coordinator::ClientState Coordinator::HandleRequest(
     const pub_sub_messages::Reconnect &request) {
+  DetermineParticipant(request.participant_id());
+  DetermineMessenger(request.participant_id());
+  LOG_F(INFO, "Handling a reconnect command from Participant %i.",
+        request.participant_id());
+  auto participant = DetermineParticipant(request.participant_id());
+  auto messenger = DetermineMessenger(request.participant_id());
   // reconnect this participant via registrar.
-  registrar_->ConnectParticipant(participant_);
+  registrar_->ConnectParticipant(participant);
   // send any missed messages satisfying the time threshold.
   Timestamp timestamp = std::chrono::steady_clock::now();
-  messenger_->SendMissedMessages(timestamp);
+  messenger->SendMissedMessages(timestamp);
   return ClientState::ACTIVE;
 }
 
 Coordinator::ClientState Coordinator::HandleRequest(
     const pub_sub_messages::SendMulticast &request) {
+  DetermineParticipant(request.participant_id());
+  LOG_F(INFO, "Handling a msend command from Participant %i.",
+        request.participant_id());
+  auto participant = DetermineParticipant(request.participant_id());
   // create and broadcast the message.
   MessageLog::Message message;
   Timestamp timestamp;
   message.timestamp = timestamp;
   message.msg = request.message();
-  message.participant_id = participant_.id;
+  message.participant_id = participant.id;
   // ensures that messages get sent in the order they were received.
   msg_queue_->Push(message);
-  if (!messenger_mgr_->BroadcastMessage(msg_queue_->Pop())) {
+  coordinator::MessageLog::Message msg = msg_queue_->Pop();
+  if (!messenger_mgr_->BroadcastMessage(&msg)) {
     LOG_F(ERROR, "Error broadcasting this message from participant (%i)",
-          participant_.id);
+          participant.id);
   }
   return ClientState::ACTIVE;
 }
@@ -190,7 +203,7 @@ uint32_t Coordinator::GenerateID() {
   do {
     id_++;
     // ensure this ID is not already taken.
-    for (auto participant :
+    for (const auto& participant :
          registrar_->GetConnectedParticipants()->GetParticipants()) {
       if (participant.id == id_) {
         duplicate = true;
@@ -204,8 +217,9 @@ uint32_t Coordinator::GenerateID() {
   return id_;
 }
 bool Coordinator::SendRegistrationResponse(
-    pub_sub_messages::RegistrationResponse &response) {
-  response.set_participant_id(participant_.id);
+    pub_sub_messages::RegistrationResponse &response,
+    const ConnectedParticipants::Participant& participant) {
+  response.set_participant_id(participant.id);
   // Serialize the message.
   if (!wire_protocol::Serialize(response, &outgoing_message_buffer_)) {
     LOG_F(ERROR, "Failed to serialize message.");
