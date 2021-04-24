c47fc40b20c7548a324422af5803a00cc0ee3bcd
Jake Chandler <jake.chandler@uga.edu>
2021-04-24 17:34:40 -0400

    add comments, remove generic `HandleRequest` from bootstrap.h

--- project4/name_server/bootstrap.h
+++ project4/name_server/bootstrap.h
@@ -16,17 +16,13 @@ namespace nameserver {
 class Bootstrap : public Nameserver {
  public:
   /**
-   * @brief Initializes
+   * @brief Initializes the bootstrap server from config file parameters.
    * @param the config file of this bootstrap
    */
   explicit Bootstrap(const std::filesystem::path config_file);
 
   /**
-   * @param Generic BootstrapMessage request
-   */
-  void HandleRequest(const consistent_hash_msgs::BootstrapMessage &request);
-
-  /**
+   * @brief Handles an entrance request from an entering name server.
    * @param A BootstrapMessage request
    */
   void HandleRequest(const consistent_hash_msgs::EntranceRequest &request);
